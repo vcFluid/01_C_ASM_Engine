fe2e53928f8d152e3bdccafb30a21e250a959aab
Scarborough <scarborough_426@163.com>
2026-05-21 18:20:48 +0800

    Homework_2 Comp

--- 02_CFD/1-D_Riemann_AM.c
+++ 02_CFD/1-D_Riemann_AM.c
@@ -1,130 +1,293 @@
-/* 半解析方案 */
-/*
-
-由于这种方案我们是求解通过特征线理论分析Riemann 1D Problem得到的超越方程（相应解是一个半解析的稳态解），不是数值格式的离散方程，所以我们不需要考虑数值格式的稳定性问题（如CFL条件），
-也不需要考虑数值耗散和数值色散等问题。我们只需要根据初始条件，关注相应超越方程的求解过程，确保求解器能够正确地找到满足方程的解即可.
-从本质上来讲，这种方案更像在找一个方程的根，而非在进行数值模拟. 所以关键在于 **数值求解一个超越方程**
-
-此外，这个超越方程描述的是稳态解，所以还需要特征线理论的分析来确定过渡状态的解，才能正确地构建整个解的结构（如激波、稀疏波等）。
-因此，虽然我们不需要担心数值格式的稳定性，但我们需要确保求解器能够正确地处理超越方程的非线性特征，并且能够找到正确的根来描述物理现象。
-
-*/
-
 #include <stdio.h>
 
 #include <stdlib.h>
 
 #include <math.h>
 
-#include <string.h>
+#define GAMMA 1.4
 
-//物理常数
-#define GAMMA 1.4 //比热比
+#define XMAX 1.0
+#define XMIN -1.0
 
-//初始状态(t=0)
-double rho_L = 1.0;
-double u_L = 0.0;   //注意: u_1 和 u_2 都是0，但u_1 和 u_2 的初始值也可以是非零的，我们这里根据教材，选择了静止的初始状态，是为了简化问题（实际上我的程序也能求解非零情况~~~~~~）
-double p_L = 1.0;
-
-double rho_R = 0.125; 
-double u_R = 0.0;     
-double p_R = 0.1;     
-
-//注意到main中应该用一个排序算法确定 状态1和 状态2！！！！！
-
-//定义超越方程的表达式 教材p77 式3.6.17
-double f_1(double p_mid, double p_j, double rho_j) { //虽然已经定义了全局变量，但我们可以仍然完整罗列出函数的参数，保持函数的独立性和通用性，一个是良好编程习惯，另一个是便于main函数统一求解五种状态的解，而不用担心全局变量的干扰。
-    double a = sqrt(GAMMA * p_j / rho_j); //声速
-    double A = (GAMMA + 1.0) / (2.0 * GAMMA);
-    double B = (GAMMA - 1.0) / (2.0 * GAMMA);
-    double P = p_mid / p_j; //压力比
-
-    double result_Shockwave, result_ExpansionWave;
-    result_Shockwave = (p_mid - p_j) / (rho_j * a * sqrt(A*P + B));
-    result_ExpansionWave = 2.0 * a / (GAMMA - 1.0) * (pow(P, (GAMMA - 1.0) / (2.0 * GAMMA)) - 1.0);
-
-    if (P > 1.0) { 
-        return result_Shockwave;
+// 左右行激波和稀疏波关系统一式子 教材p77 式3.6.17
+double f_wave(double p_mid, double p_j, double rho_j) {
+    double a_j = sqrt(GAMMA * p_j / rho_j);
+    if (p_mid > p_j) { 
+        double A = rho_j * a_j * sqrt((GAMMA + 1.0) / (2.0 * GAMMA) * (p_mid / p_j) + (GAMMA - 1.0) / (2.0 * GAMMA));
+        return (p_mid - p_j) / A;
     } else { 
-        return result_ExpansionWave;
+        return (2.0 * a_j / (GAMMA - 1.0)) * (pow(p_mid / p_j, (GAMMA - 1.0) / (2.0 * GAMMA)) - 1.0);
     }
+}//注意到公式在p_mid = p_j 时是连续的，且函数值为0.
+
+double sound_speed(double p, double rho) {
+    return sqrt(GAMMA * p / rho);
 }
 
-//定义不同波区的解的结构求解C语言函数
-//此时相当于得到了两个边值条件，如何求解流场内部物理场的分布 参考ppt Lesson02 p76
-double field_constant(double p_mid, double u_1, double rho_1, double p_1, double u_2, double rho_2, double p_2)//常数状态区域的求解 常数状态区,alpha恒等于alpha_0, beta恒等于beta_0,所以我们可以直接通过初始状态的alpha和beta来求解常数状态区的物理量分布
+double calcul_A (double p_mid, double p_j, double rho_j) 
 {
-    double alpha_1 = u_1 + 2/(GAMMA - 1.0) * sqrt(GAMMA * p_1 / rho_1);
-    double beta_1 = u_1 - 2/(GAMMA - 1.0) * sqrt(GAMMA * p_1 / rho_1);
-
-    double alpha_2 = u_2 + 2/(GAMMA - 1.0) * sqrt(GAMMA * p_2 / rho_2);
-    double beta_2 = u_2 - 2/(GAMMA - 1.0) * sqrt(GAMMA * p_2 / rho_2);  
-
-    double u_x1 = 0.5 * (alpha_1 + beta_1);
-    double u_x2 = 0.5 * (alpha_2 + beta_2);
-
-    double c_x1 = (GAMMA-1)/4.0 * (alpha_1 - beta_1);
-    double c_x2 = (GAMMA-1)/4.0 * (alpha_2 - beta_2);
-
-    return u_x1, u_x2;
+    double a_j = sqrt(GAMMA * p_j / rho_j);
+    double A = rho_j * a_j * sqrt((GAMMA + 1.0) / (2.0 * GAMMA) * (p_mid / p_j) + (GAMMA - 1.0) / (2.0 * GAMMA));
+    return A;
 }
 
-int main(){
-    //由于教材中在判断解的状态时，假设了p1 <= p2，所以需要先用一个排序算法来确定状态1和状态2，确保p_1 <= p_2
-    double p_1, p_2, u_1, u_2, rho_1, rho_2;
-    if (p_L <= p_R) {
-        p_1 = p_L;
-        u_1 = u_L;
-        rho_1 = rho_L;
+int main() {
+    system("chcp 65001 > nul");
+  //初始条件
+    double rho_L = 0.445, u_L = 0.698, p_L = 3.528;
+    double rho_R = 0.5, u_R = 0.0, p_R = 0.571;
+    double x_0 = 0.5;
 
-        p_2 = p_R;
-        u_2 = u_R;
-        rho_2 = rho_R;
-    } else {
-        p_1 = p_R;
-        u_1 = u_R;
-        rho_1 = rho_R;
+    double time;
 
-        p_2 = p_L;
-        u_2 = u_L;
-        rho_2 = rho_L;
+    printf("  　 　 ＿＿＿\n");
+    printf("  　 ／＞　　フ\n");
+    printf("    | 　_　 _ |\n");
+    printf("   ／` ミ＿xノ\n");
+    printf("   /　　　 　 |\n");
+    printf("  /　 ヽ　　 ﾉ\n");
+    printf(" │　 　|　|　|\n");
+    printf("／￣   |  |　|\n");
+    printf("| (￣ヽ＿_ヽ_)__)\n");
+    printf("＼二つ                \n");
+
+    printf("==================================================\n");
+    printf("      1-D Riemann Solver - Initial Conditions    \n");
+    printf("==================================================\n");
+
+    printf("Step 1: Input LEFT state (rho_L  u_L  p_L)\n");
+    printf("        [Separate by spaces and then press Enter]: ");
+    if (scanf("%lf %lf %lf", &rho_L, &u_L, &p_L) != 3) {
+        printf("\n[Error]!\n");
+        return 1;
     }
 
+    printf("\n");
 
-    //我们首先需要求解一个超越方程，形式为u_1 - u_2 = f_1(p_middle, p_1, rho_1) - f_1(p_middle, p_2, rho_2) = 0，其中 p_middle 是我们要求解的未知数，代表面两侧的压力。 教材p77 3.6.18
-    double p_mid; //待求量
+    printf("Step 2: Input RIGHT state (rho_R  u_R  p_R)\n");
+    printf("        [Separate by spaces and press Enter]: ");
+    if (scanf("%lf %lf %lf", &rho_R, &u_R, &p_R) != 3) {
+        printf("\n[Error]!\n");
+        return 1;
+    }
 
-    double F2 = f_1(p_2, p_1, rho_1) + f_1(p_2, p_2, rho_2); //用于依照教材方法判断激波or系数波；这里严格满足教材的假设，即p_1 <= p_2，所以直接调用我们封装好的函数，并将p_2作为f_1函数的输入，来判断解的结构（激波还是稀疏波）
-    double F1 = f_1(p_1, p_2, rho_2) + f_1(p_1, p_1, rho_1); //同上
-    double F0 = f_1(0, p_1, rho_1) + f_1(0, p_2, rho_2); //用于判断解的结构是激波还是稀疏波；这里我们直接调用f_1函数，并将p_1作为输入，来判断解的结构（激波还是稀疏波）
-    printf("The value of F2 is: %f ", F2);
-    printf("The value of F1 is: %f ", F1);
-    printf("The value of F0 is: %f ", F0);
-    if (u_1 - u_2 > F2 || u_1 - u_2 == F2) {   
-        printf("Both sides are shock waves.\n");
-        p_mid = p_2;
+    printf("Step 3: Input Time variation\n");
+    printf("        [Press Enter to continue]: ");
+    if (scanf("%lf", &time) < 0){
+        printf("\n[Error]!\n");
+        return 0;
     }
-    else if (F2 > u_1 - u_2 &&(u_1 - u_2 > F1 || u_1 - u_2 == F1)) {
-        printf("One side is shock wave, the other side is expansion wave.\n");
-    }
-    else if (F1 > u_1 - u_2 && (u_1 - u_2 > F0 || u_1 - u_2 == F0)) {
-        printf("Both sides are expansion waves.\n");
-    }
-    else if (F0 > u_1 - u_2) {
-        printf("The solution is vacuum.\n");
-    }
-    else{
-        printf("The solution is unphysical.Maybe the error is in the judgment logic.\n");
-    }
-    
 
-    //然后需要利用求解得到的 p_mid 来计算过渡状态的速度 u_X 和密度 rho_x
+    printf("\n==================================================\n");
+    printf("Initialization successful! Lets Start solver QwQ...\n\n");
 
-    double u_mid, rho_mid;
-    u_mid = 1/2.0 * (u_1 + u_2 - f_1(p_mid, p_1, rho_1) + f_1(p_mid, p_2, rho_2)); //根据教材p79 式3.6.26
-    printf("The solution of mid-velocity is: %f ", u_mid);
-    //由于我们得到的是间断速度，还需要通过特征线理论来分析解的结构，进而得到整个解的分布情况（如激波、稀疏波等）
+    //计算初始声速，用于后续① 判断真空条件（实际上是f_wave(0)的值） ② 求波头、波尾速度
+    double a_L = sound_speed(p_L, rho_L);
+    double a_R = sound_speed(p_R, rho_R);
 
+    //真空条件判定 (Vacuum Check)
+    double F_0 = f_wave(0.0, p_L, rho_L) + f_wave(0.0, p_R, rho_R); //对应p78 F(0)的值
+    int is_vacuum = 0;
+    if (F_0 >= u_L - u_R) {
+        printf("Vacuum Generation!\n");
+        is_vacuum = 1;
+    }
+
+    double p_mid = 0.0, u_mid = 0.0;
+    double rho_mid_L = 0.0, rho_mid_R = 0.0;
+    double a_mid_L = 0.0, a_mid_R = 0.0;
+
+    double Z_L = 0.0, Z_R = 0.0; //特征线速度初始化
+
+    if (!is_vacuum) {
+        double p_low = 0.0;
+        double p_high = fmax(p_L, p_R); 
+
+        while (f_wave(p_high, p_L, rho_L) + f_wave(p_high, p_R, rho_R) + (u_R - u_L) < 0) {
+            p_high *= 2.0;
+        }
+
+        //二分法找根
+        double tol = 1e-8;
+        while ((p_high - p_low) > tol) {
+            p_mid = 0.5 * (p_low + p_high);
+            double F_mid = f_wave(p_mid, p_L, rho_L) + f_wave(p_mid, p_R, rho_R) + (u_R - u_L);
+            if (F_mid > 0) p_high = p_mid;
+            else p_low = p_mid;
+        }
+        printf("The solution of p_mid is: %f \n", p_mid);
+
+        //计算中间区间断解的速度和密度 
+        u_mid = 0.5 * (u_L + u_R) + 0.5 * (f_wave(p_mid, p_R, rho_R) - f_wave(p_mid, p_L, rho_L));
+
+        printf("The solution of u_mid is: %f \n", u_mid);
+
+        /*
+        截至目前，我们已经得到了中间区域的pressure p_mid 和 velocity u_mid，接下来我们需要根据p_mid和u_mid来判断中间区域的密度 rho_mid_L 和 rho_mid_R 的计算方式（是激波还是稀疏波），并计算出它们的数值。
+        */
+        if (p_mid > p_L) {
+            double A_L = calcul_A(p_mid, p_L, rho_L);
+            rho_mid_L = rho_L * A_L  / (A_L - rho_L * (u_L - u_mid)); //根据教材p78 3.6.27 来计算中间区的密度
+            printf("Left is shock wave, rho_mid_L is: %f \n", rho_mid_L);
+            Z_L = u_L - A_L / rho_L; //根据教材p79 3.6.26 来计算特征线的速度
+            //虽然求解解的结构不需要Z_1的数值，但我们可以利用Z_1自适应地调整x轴采样宽度
+        }
+        else {
+            a_mid_L = a_L + (GAMMA - 1.0) / 2.0 * (u_L - u_mid); 
+            rho_mid_L = GAMMA*p_mid / (a_mid_L * a_mid_L); // p78 3.6.29a
+            printf("Left is expansion wave, rho_mid_L is: %f \n", rho_mid_L);
+            Z_L = u_L - a_L;
+        } 
+
+        if (p_mid > p_R) {
+            double A_R = calcul_A(p_mid, p_R, rho_R);
+            rho_mid_R = rho_R * A_R  / (A_R + rho_R * (u_R - u_mid)); //根据教材p78 3.6.27 来计算中间区的密度
+            printf("Right is shock wave, rho_mid_R is: %f \n", rho_mid_R);
+            Z_R = u_R + A_R / rho_R;
+        }
+        else {
+            a_mid_R = a_R - (GAMMA - 1.0) / 2.0 * (u_R - u_mid);
+            rho_mid_R = GAMMA*p_mid / (a_mid_R * a_mid_R); // p78 3.6.29a
+            printf("Right is expansion wave, rho_mid_R is: %f \n", rho_mid_R);
+            Z_R = u_R + a_R;
+        }
+    }
+
+    if (Z_R - Z_L < 0.1) {
+        printf("Warning! The wave structure is very narrow, please adjust the initial conditions to ensure a more clear wave structure for better visualization and analysis.\n");
+    }
+
+    double t = time;
+    double x_min, x_max; 
+    x_min = XMIN - (Z_R - Z_L)* t * 1.2;
+    printf ("Z_R-Z_L = %f\n", Z_R - Z_L);
+    printf ("x_min = %f\n", x_min);
+    x_max = XMAX + (Z_R - Z_L)* t * 1.2; //这样取是为了确保能够采样到完整的解结构，是自适应思想的朴素形态
+    printf ("x_max = %f\n", x_max);
+    double dx = 0.002;
+    int N_points = (int)((x_max - x_min) / dx) + 1; //采用int类型来确保N_points是一个整数，避免由于浮点数计算误差导致的采样点数不准确问题。
+
+    FILE *fp = fopen("riemann_exact_tecplot.dat", "w"); 
+    if (fp == NULL) {
+        printf("无法创建输出文件！\n");
+        return 1;
+    }
+    fprintf(fp, "TITLE = \"Correct 1D Riemann Exact Solution\"\n"); 
+    fprintf(fp, "VARIABLES = \"X\", \"Density\", \"Velocity\", \"Pressure\"\n"); 
+    fprintf(fp, "ZONE T=\"Exact\", I=%d, F=POINT\n", N_points); 
+
+    //特征线采样循环 (整数循环保证 N_points 严格对应表头)
+    for (int i = 0; i < N_points; i++) {
+        double x = x_min + i * dx; // 反推精准 x 坐标
+        double rho_out = 0, u_out = 0, p_out = 0;
+        double xi = (x - x_0) / t; // 由物理坐标反推参考坐标 xi ,不用t - t_0是因为我们的物理建模默认t_0 = 0
+
+        if (is_vacuum) {
+            //真空特解
+            double Z_L_tail = u_L + 2.0 * a_L / (GAMMA - 1.0); //教材 p81 3.6.42
+            double Z_R_tail = u_R - 2.0 * a_R / (GAMMA - 1.0);
+            
+            //其他逻辑与两侧都是稀疏波的情况相同
+            double Z_L_head = u_L - a_L;
+            double Z_R_head = u_R + a_R;   
+
+            if (xi <= Z_L_head) { //左未扰动，注意到参考坐标的定义已经除以了时间t，所以这里直接比较xi和Z_L_head即可
+                rho_out = rho_L; u_out = u_L; p_out = p_L;
+
+            } else if (xi > Z_L_head && xi < Z_L_tail) { // 左稀疏区
+                u_out = (2.0 / (GAMMA + 1.0)) * (a_L + (GAMMA - 1.0) / 2.0 * u_L + xi); // 公式由特征线理论得来，详情见笔记
+                double a_current = u_out - xi;
+                p_out = p_L * pow(a_current / a_L, 2.0 * GAMMA / (GAMMA - 1.0));
+                rho_out = GAMMA * p_out / (a_current * a_current);
+
+            } else if (xi >= Z_L_tail && xi <= Z_R_tail) { // 真空
+                rho_out = 0.0; u_out = 0.0; p_out = 0.0;
+                
+            } else if (xi > Z_R_tail && xi < Z_R_head) { // 右稀疏
+                u_out = (2.0 / (GAMMA + 1.0)) * (-a_R + (GAMMA - 1.0) / 2.0 * u_R + xi);
+                double a_current = xi - u_out;
+                p_out = p_R * pow(a_current / a_R, (2.0 * GAMMA) / (GAMMA - 1.0));
+                rho_out = GAMMA * p_out / (a_current * a_current);
+
+            } else { // 右未扰动
+                rho_out = rho_R; u_out = u_R; p_out = p_R;
+            } 
+        } 
+
+        else {
+            //正常中间区解
+            if (xi < u_mid) {
+                //接触间断左侧
+                if (p_mid < p_L) { // 左侧稀疏波
+                    double a_star_L = a_L + (GAMMA - 1.0) / 2.0 * (u_L - u_mid); //教材p80 3.6.29b
+                    double Z_L_head = u_L - a_L; //这里重新算了一遍，虽然之前在求解p_mid时已经判断过了，更好的做法是把这个结果保存下来，避免重复计算，但目前能跑通就行，后续再优化
+                    double Z_L_tail = u_mid - a_star_L;
+                    if (xi < Z_L_head) {
+                        rho_out = rho_L; u_out = u_L; p_out = p_L;
+                    } else if (xi >= Z_L_head && xi < Z_L_tail) { //系数波区
+                        u_out = (2.0 / (GAMMA + 1.0)) * (a_L + (GAMMA - 1.0) / 2.0 * u_L + xi);
+                        double a_current = u_out - xi; // 因为左行特征线满足 u - a = xi 
+                        p_out = p_L * pow(a_current / a_L, (2.0 * GAMMA) / (GAMMA - 1.0));
+                        rho_out = GAMMA * p_out / (a_current * a_current);
+                    } else {
+                        rho_out = rho_mid_L; 
+                        u_out = u_mid; 
+                        p_out = p_mid;
+                    }
+                } else { // 左侧激波
+                    double A_L = calcul_A(p_mid, p_L, rho_L);
+                    double Z_L_shock = u_L - A_L / rho_L;
+                    if (xi < Z_L_shock) {
+                        rho_out = rho_L; u_out = u_L; p_out = p_L;
+                    } else {
+                        rho_out = rho_mid_L; u_out = u_mid; p_out = p_mid;
+                    }
+                }
+            } else {
+                //接触间断右侧
+                if (p_mid > p_R) { //右侧激波
+                    double A_R = calcul_A(p_mid, p_R, rho_R);
+                    double Z_R_shock = u_R + A_R / rho_R;
+                    if (xi >= Z_R_shock) {
+                        rho_out = rho_R; 
+                        u_out = u_R; 
+                        p_out = p_R;
+                    } else {
+                        rho_out = rho_mid_R; u_out = u_mid; p_out = p_mid;
+                    }
+                } else { //右侧稀疏波
+                    double a_star_R = a_R - (GAMMA - 1.0) / 2.0 * (u_R - u_mid); //教材p80 3.6.37a
+                    double Z_R_head = u_R + a_R;
+                    double Z_R_tail = u_mid + a_star_R;
+                    
+                    if (xi >= Z_R_head) { 
+                        rho_out = rho_R; u_out = u_R; p_out = p_R;
+                    } else if (xi > Z_R_tail && xi < Z_R_head) {
+                        u_out = (2.0 / (GAMMA + 1.0)) * (-a_R + (GAMMA - 1.0) / 2.0 * u_R + xi);
+                        double a_current = xi - u_out; 
+                        p_out = p_R * pow(a_current / a_R, (2.0 * GAMMA) / (GAMMA - 1.0));
+                        rho_out = GAMMA * p_out / (a_current * a_current);
+                    } else {
+                        rho_out = rho_mid_R; u_out = u_mid; p_out = p_mid;
+                    }
+                }
+            }
+        }
+        //写入数据行
+        fprintf(fp, "%f\t%f\t%f\t%f\n", x, rho_out, u_out, p_out);
+    }
+
+    fclose(fp);
+    printf("Tecplot data generated successfully!\n");
+    if (!is_vacuum) {
+        printf("Validation -> p_mid: %f, u_mid: %f\n", p_mid, u_mid);
+    }
     return 0;
 }
+
+/*
+值得优化的点：
+①用更优秀的迭代方案（例如牛顿迭代法）
+②将物理量和方法封装为结构体，优化代码结构
+*/
 
